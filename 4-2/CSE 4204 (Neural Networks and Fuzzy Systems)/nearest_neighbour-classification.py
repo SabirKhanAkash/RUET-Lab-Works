4e9a7bbf6e2db12f735f0f8542dd92d7c3099b9f
Sabir Khan Akash <4175.akash@gmail.com>
2022-04-07 16:23:15 +0600

    NNFS lab 1 code 1 added

---
+++ 4-2/CSE 4204 (Neural Networks and Fuzzy Systems)/nearest_neighbour-classification.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import math
+
+football_height=[189];
+football_weight=[85];
+wwe_height=[220];
+wwe_weight=[120];
+
+temp_f_dist = [];
+temp_w_dist = [];
+
+count = 1;
+
+while(1):
+    for i in range(True):
+
+        in_height = int(input("Player Height: "));
+        in_weight = int(input("Player Weight: "));
+        count = count + 1;
+
+        if(i>0):
+            for j in range(count):
+                temp_f_dist.append(math.sqrt(((abs(in_height-football_height[j])*abs(in_height-football_height[j]))+(abs(in_weight-football_weight[j])*abs(in_weight-football_weight[j])))))
+                temp_w_dist.append(math.sqrt(((abs(in_height-wwe_height[j])*abs(in_height-wwe_height[j]))+(abs(in_weight-wwe_weight[j])*abs(in_weight-wwe_weight[j])))))
+
+            a = float(min(temp_f_dist));
+            b = float(min(temp_w_dist));
+            print("Football: "+temp_f_dist+",");
+            print("WWE: "+temp_w_dist+",");
+        elif(i==0):
+            a = math.sqrt(((abs(in_height-football_height[0])*abs(in_height-football_height[0]))+(abs(in_weight-football_weight[0])*abs(in_weight-football_weight[0]))))
+            b = math.sqrt(((abs(in_height-wwe_height[0])*abs(in_height-wwe_height[0]))+(abs(in_weight-wwe_weight[0])*abs(in_weight-wwe_weight[0]))))
+
+        if(a<b):
+            football_height.append(in_height);
+            football_weight.append(in_weight);
+
+        else:
+            wwe_height.append(in_height);
+            wwe_weight.append(in_weight);
+
+
+    plt.scatter(football_height,football_weight,label="Football Players",color="green",marker=".",s=100);
+    plt.scatter(wwe_height,wwe_weight,label="WWE Players",color="red",marker="*",s=100);
+    plt.xlabel("Player's Height");
+    plt.ylabel("Player's Weight");
+    plt.title('Nearest Neighbour Classification with Rogue Pattern');
+    plt.legend();
+
+    plt.show();
+
+    print("Football: "+temp_f_dist+",");
+    print("WWE: "+temp_w_dist+",");
