2d0a4e72a7f63045c66ce6b7909f3054d4323a5c
Sabir Khan Akash <4175.akash@gmail.com>
2022-08-26 23:54:41 +0600

    hopfield code updated

--- 4-2/CSE 4204 (Neural Networks and Fuzzy Systems)/Lab 4/hopfield_nn_algorithm.py
+++ 4-2/CSE 4204 (Neural Networks and Fuzzy Systems)/Lab 4/hopfield_nn_algorithm.py
@@ -10,8 +10,9 @@ Original file is located at
 import math
 import numpy as np, numpy.random
 
-inp = int(input("How many input features: "))
+inp = int(input("How many inputs: "))
 feature = math.ceil(inp * float(10/15))
+# feature = int(input("How many features per input: "))
 
 rando = [1,-1]
 inputs = []
@@ -27,20 +28,30 @@ def calc(p,q):
 def checkMatch(uPatt, inps):
     m = 0
     t = 0
+    listo = []
     for m in range(inp):
         t = list(np.array(uPatt == inps[m][0]).reshape(-1,))
+        backup = m
+        listo.append(t.count(True))
         if (t.count(True) == feature):
             print("\t---> Matched ", t.count((True)))
-            return m
-    print("\t---> Matched ", t.count((True)))
-    return False
+            return False, t.count(True), backup
+
+    max = -999999
+    meo = -1
+    for xx in range(len(listo)):
+        if listo[xx] > max:
+            max = listo[xx]
+            meo = xx
+    print("\t---> Matched ", max)
+    return False, max, meo
 
 def update(u,up,weight):
     res = np.matmul(u,weight[:,up])
-    if(res >= 0):
+    if(res > 0):
         return 1
     else:
-        return 0 - 1
+        return -1
 
 print("\n<------------- Inputs ------------->")
 
@@ -64,20 +75,22 @@ print(unknownPattern)
 
 pp = 0
 c = 0
+prev = -1
 while(1):
     print("\n<------------------------","Testing Cycle ",c,"------------------------>")
     c=c+1
     print("Updated Unknown Pattern = ",unknownPattern, end="")
-    final = checkMatch(unknownPattern,inputs)
-    if(final == False):
+    
+    final, nvm, bc = checkMatch(unknownPattern,inputs)
+    if(prev == nvm):
+        print("\n<------------------ Testing Completed ------------------>")
+        print("Final Unknown Pattern Matches with input pattern P", bc)
+        break;
+    else:
         uv = unknownPattern[pp]
         unknownPattern[pp] = (update(unknownPattern, pp, w))
-
+        prev = nvm
         if (pp == (feature - 1)):
             pp = 0
         else:
             pp = pp + 1
-    else:
-        print("\n<------------------ Testing Completed ------------------>")
-        print("Final Unknown Pattern Matches with input pattern P", final)
-        break;
